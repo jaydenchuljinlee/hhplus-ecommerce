2e705310738669ccfd36667b118d51c4140848bf
이철진 <ironjin92@joonggonara.co.kr>
2024-10-18 00:35:19 +0900

    Updated: 잔액 기능 API 연결

--- src/main/kotlin/com/hhplus/ecommerce/api/balance/dto/BalanceViewResponse.kt
+++ src/main/kotlin/com/hhplus/ecommerce/api/balance/dto/BalanceViewResponse.kt
@@ -1,7 +1,15 @@
 package com.hhplus.ecommerce.api.balance.dto
 
+import com.hhplus.ecommerce.domain.balance.dto.BalanceResult
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.media.Schema
+
 class BalanceViewResponse(
+    @Parameter(description = "사용자 ID", required = true)
+    @Schema(description = "사용자 ID", example = "1")
     var userId: Long,
+    @Parameter(description = "현재 잔액", required = true)
+    @Schema(description = "현재 잔액", example = "1000")
     var balance: Long
 ) {
     companion object {
@@ -9,5 +17,12 @@ class BalanceViewResponse(
             userId = 0L,
             balance = 1000L
         )
+
+        fun from(result: BalanceResult): BalanceViewResponse {
+            return BalanceViewResponse(
+                userId = result.userId,
+                balance = result.balance,
+            )
+        }
     }
 }
