8643dad51608eafaf69a1768255730bffd22ac2f
이철진 <ironjin92@joonggonara.co.kr>
2024-10-22 21:25:58 +0900

    Added: CORS 필터 적용

---
+++ src/main/kotlin/com/hhplus/ecommerce/common/config/WebMvcConfig.kt
@@ -0,0 +1,16 @@
+package com.hhplus.ecommerce.common.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebMvcConfig: WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOriginPatterns("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+            .allowCredentials(true)
+        super.addCorsMappings(registry)
+    }
+}
