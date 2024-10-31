60d8d36f7444947e853e8271f0bcf7ca2729f089
이철진 <ironjin92@joonggonara.co.kr>
2024-11-01 02:15:00 +0900

    Updated: 상품 재고 관련 Lock 적용

--- src/main/kotlin/com/hhplus/ecommerce/domain/product/ProductService.kt
+++ src/main/kotlin/com/hhplus/ecommerce/domain/product/ProductService.kt
@@ -3,15 +3,18 @@ package com.hhplus.ecommerce.domain.product
 import com.hhplus.ecommerce.domain.product.dto.*
 import com.hhplus.ecommerce.domain.product.repository.IProductRepository
 import com.hhplus.ecommerce.infrastructure.product.dto.BestSellingProduct
+import com.hhplus.ecommerce.infrastructure.redis.PubSubLockSupporter
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import java.util.concurrent.TimeUnit
 
 @Service
 class ProductService(
-    private val productRepository: IProductRepository
+    private val productRepository: IProductRepository,
+    private val pubSubLockSupporter: PubSubLockSupporter
 ) {
     fun getProduct(dto: ProductInfoQuery): ProductInfoResult {
-        val productDetailEntity = productRepository.findByProductIdWithLock(dto.productId)
+        val productDetailEntity = productRepository.findByProductId(dto.productId)
         val productEntity = productRepository.findById(dto.productId)
 
         val result = ProductInfoResult(
@@ -23,15 +26,30 @@ class ProductService(
         return result
     }
 
-    fun decreaseStock(dto: DecreaseProductDetailStock): ProductDetailResult {
-        val productDetailEntity = productRepository.decreaseStock(dto.id, dto.amount)
 
-        return ProductDetailResult.from(productDetailEntity)
+    fun decreaseStock(dto: DecreaseProductDetailStock): ProductDetailResult {
+        val productKey = "product:${dto.id}"
+
+        val lock = pubSubLockSupporter.getLock(productKey)
+
+        try {
+            val acquireLock = lock.tryLock(10, 1, TimeUnit.SECONDS)
+            if (!acquireLock) {
+                throw InterruptedException("Lock 획득 실패")
+            }
+
+            val productDetailEntity = productRepository.decreaseStock(dto.id, dto.amount)
+
+            return ProductDetailResult.from(productDetailEntity)
+
+        } finally {
+            lock.unlock()
+        }
     }
 
     @Transactional
-    fun getProductDetailByIdWithLock(item: ProductDetailQuery): ProductDetailResult {
-        val entity = productRepository.findByProductIdWithLock(item.productId)
+    fun getProductDetail(item: ProductDetailQuery): ProductDetailResult {
+        val entity = productRepository.findByProductId(item.productId)
         return ProductDetailResult.from(entity)
     }
 
