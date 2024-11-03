0b5940e9f89b0f190a73618cbb38073d4a4d0bc1
이철진 <ironjin92@joonggonara.co.kr>
2024-11-03 17:38:59 +0900

    Updated: Redisson 설정 변경

---
+++ src/main/kotlin/com/hhplus/ecommerce/common/config/RedissonConfig.kt
@@ -0,0 +1,22 @@
+package com.hhplus.ecommerce.common.config
+
+import org.redisson.Redisson
+import org.redisson.api.RedissonClient
+import org.redisson.config.Config
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class RedissonConfig {
+
+    @Bean
+    fun redissonClient(): RedissonClient {
+        val config = Config()
+        config.useSingleServer()
+            .setAddress("redis://localhost:6379") // Docker에서 Redis가 실행 중인 호스트
+            .setConnectionMinimumIdleSize(10)
+            .setConnectionPoolSize(64)
+
+        return Redisson.create(config)
+    }
+}
