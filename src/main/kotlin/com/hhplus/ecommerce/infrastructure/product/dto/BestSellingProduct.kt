4ddde38ec8b4c3c045e5cd8fb8fa106ad0acbd79
이철진 <ironjin92@joonggonara.co.kr>
2024-11-07 22:30:55 +0900

    Refactor: 상품 DTO 수정 - 캐시 직렬화

--- src/main/kotlin/com/hhplus/ecommerce/infrastructure/product/dto/BestSellingProduct.kt
+++ src/main/kotlin/com/hhplus/ecommerce/infrastructure/product/dto/BestSellingProduct.kt
@@ -1,5 +1,7 @@
 package com.hhplus.ecommerce.infrastructure.product.dto
 
+import java.io.Serializable
+
 data class BestSellingProduct(
     var productId: Long = 0,
     var productName: String = "",
@@ -8,4 +10,4 @@ data class BestSellingProduct(
     var totalPayPrice: Long = 0,
     var orderCount: Long = 0,
     var payCount: Long = 0,
-)
+): Serializable
