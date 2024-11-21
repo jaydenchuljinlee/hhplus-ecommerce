6c56873b5d7d83f7e36c0949844104a8478d0428
ironjin <cheoljin1234@naver.com>
2024-11-22 01:22:11 +0900

    Updated: 아웃박스 상태 조회 메서드 추가

--- src/main/kotlin/com/hhplus/ecommerce/infrastructure/outboxevent/OutboxEventRepository.kt
+++ src/main/kotlin/com/hhplus/ecommerce/infrastructure/outboxevent/OutboxEventRepository.kt
@@ -2,6 +2,7 @@ package com.hhplus.ecommerce.infrastructure.outboxevent
 
 import com.hhplus.ecommerce.infrastructure.outboxevent.jpa.OutboxEventJpaRepository
 import com.hhplus.ecommerce.infrastructure.outboxevent.jpa.entity.OutboxEventEntity
+import com.hhplus.ecommerce.infrastructure.outboxevent.jpa.entity.enums.OutboxEventStatus
 import org.springframework.stereotype.Repository
 import java.util.UUID
 
@@ -11,6 +12,10 @@ class OutboxEventRepository(
 ) {
     fun findById(id: UUID): OutboxEventEntity {
         return outboxEventJpaRepository.findById(id).orElseThrow { IllegalArgumentException("존재하지 않는 이벤트입니다 => $id") }
+    }
+
+    fun findAllByTopicAndStatus(topic: String, status: OutboxEventStatus): List<OutboxEventEntity> {
+        return outboxEventJpaRepository.findAllByTopicAndStatus(topic, status)
     }
 
     fun insertOrUpdate(event: OutboxEventEntity): OutboxEventEntity {
--- src/main/kotlin/com/hhplus/ecommerce/infrastructure/outboxevent/jpa/OutboxEventJpaRepository.kt
+++ src/main/kotlin/com/hhplus/ecommerce/infrastructure/outboxevent/jpa/OutboxEventJpaRepository.kt
@@ -1,8 +1,10 @@
 package com.hhplus.ecommerce.infrastructure.outboxevent.jpa
 
 import com.hhplus.ecommerce.infrastructure.outboxevent.jpa.entity.OutboxEventEntity
+import com.hhplus.ecommerce.infrastructure.outboxevent.jpa.entity.enums.OutboxEventStatus
 import org.springframework.data.jpa.repository.JpaRepository
 import java.util.UUID
 
 interface OutboxEventJpaRepository: JpaRepository<OutboxEventEntity, UUID> {
+    fun findAllByTopicAndStatus(topic: String, status: OutboxEventStatus): List<OutboxEventEntity>
 }
