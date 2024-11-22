f7e2ed3ba0f007a20fa096c9bf51d51692eca4ab
ironjin <cheoljin1234@naver.com>
2024-11-23 00:59:54 +0900

    Added & Refactor: 카프카 발행 추가

---
+++ src/main/kotlin/com/hhplus/ecommerce/infrastructure/kafka/KafkaProducer.kt
@@ -0,0 +1,14 @@
+package com.hhplus.ecommerce.infrastructure.kafka
+
+import com.hhplus.ecommerce.infrastructure.outboxevent.event.dto.OutboxEventInfo
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.stereotype.Component
+
+@Component
+class KafkaProducer(
+    private val kafkaTemplate: KafkaTemplate<String, OutboxEventInfo>
+) {
+    fun sendOutboxEvent(event: OutboxEventInfo) {
+        kafkaTemplate.send(event.topic, event)
+    }
+}
