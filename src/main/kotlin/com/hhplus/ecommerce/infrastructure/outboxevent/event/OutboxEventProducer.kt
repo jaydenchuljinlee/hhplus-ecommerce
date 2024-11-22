d564a06dcccc3a886c1b7cde0b2f4a6a08651b28
ironjin <cheoljin1234@naver.com>
2024-11-23 01:01:54 +0900

    Deleted: 미사용 파일 제거

--- src/main/kotlin/com/hhplus/ecommerce/infrastructure/outboxevent/event/OutboxEventProducer.kt
+++
@@ -1,8 +0,0 @@
-package com.hhplus.ecommerce.infrastructure.outboxevent.event
-
-import com.hhplus.ecommerce.infrastructure.outboxevent.jpa.entity.OutboxEventEntity
-
-interface OutboxEventProducer {
-    fun beforeCommit(event: OutboxEventEntity)
-    fun afterCommit(event: OutboxEventEntity)
-}
