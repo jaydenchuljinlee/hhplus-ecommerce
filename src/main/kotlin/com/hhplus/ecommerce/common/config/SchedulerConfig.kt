4fdbde936182de2e610ef3456506cbb2f4ff77cd
ironjin <cheoljin1234@naver.com>
2024-11-23 01:02:21 +0900

    Updated: 스케줄링 활성화

--- src/main/kotlin/com/hhplus/ecommerce/common/config/SchedulerConfig.kt
+++ src/main/kotlin/com/hhplus/ecommerce/common/config/SchedulerConfig.kt
@@ -6,8 +6,8 @@ import org.springframework.scheduling.annotation.SchedulingConfigurer
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler
 import org.springframework.scheduling.config.ScheduledTaskRegistrar
 
-// @Configuration
-// @EnableScheduling
+@Configuration
+@EnableScheduling
 class SchedulerConfig: SchedulingConfigurer {
     override fun configureTasks(taskRegistrar: ScheduledTaskRegistrar) {
         val threadPoolTaskScheduler = ThreadPoolTaskScheduler()
