f1ca7cddb39cef91f1e7d35ea5db36f344a755e9
ironjin <cheoljin1234@naver.com>
2024-11-27 23:23:06 +0900

    Added: 부하 테스트를 위한 K6 테스트 스크립트

--- performance/perf-product-db.js
+++ performance/perf-product-db.js
@@ -3,7 +3,9 @@ import { sleep, check } from 'k6';
 
 export let options = {
     stages: [
-        { duration: '30s', target: 500 },  // 1분 동안 500명의 VU로 증가
+        { duration: '30s', target: 100 }, // 30초 동안 100명의 사용자
+        { duration: '1m', target: 500 },  // 1분 동안 500명의 사용자
+        { duration: '30s', target: 0 },   // 30초 동안 사용자 종료
     ],
 };
 
@@ -14,7 +16,7 @@ export default function () {
     let dbRes = http.get(`${BASE_URL}/product/db?productId=1`);
     check(dbRes, {
         'DB status is 200': (r) => r.status === 200,
-        'DB response time < 500ms': (r) => r.timings.duration < 500,
+        'DB response time < 500ms': (r) => r.timings.duration < 200,
     });
 
     sleep(1); // 1초 간격으로 반복
