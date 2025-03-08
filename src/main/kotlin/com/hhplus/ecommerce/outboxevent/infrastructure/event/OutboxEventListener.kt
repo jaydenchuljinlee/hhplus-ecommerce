57737e04b669b4955076792bfc4da8387ac95668
ironjin <cheoljin1234@naver.com>
2025-03-08 14:25:41 +0900

    Refactor: ApplicationEvent 병합

--- src/main/kotlin/com/hhplus/ecommerce/outboxevent/infrastructure/event/OutboxEventListener.kt
+++ src/main/kotlin/com/hhplus/ecommerce/outboxevent/infrastructure/event/OutboxEventListener.kt
@@ -1,22 +1,31 @@
 package com.hhplus.ecommerce.outboxevent.infrastructure.event
 
-import com.hhplus.ecommerce.outboxevent.domain.OutboxEventService
 import com.hhplus.ecommerce.infrastructure.kafka.KafkaProducer
+import com.hhplus.ecommerce.outboxevent.domain.OutboxEventService
 import com.hhplus.ecommerce.outboxevent.infrastructure.event.dto.OutboxEventInfo
 import com.hhplus.ecommerce.outboxevent.infrastructure.jpa.entity.enums.OutboxEventStatus
 import org.slf4j.LoggerFactory
+import org.springframework.context.event.EventListener
+import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
 import org.springframework.transaction.event.TransactionPhase
 import org.springframework.transaction.event.TransactionalEventListener
 
-// @Component
-class OutboxAfterEventListener(
+@Component
+class OutboxEventListener(
     private val outboxEventService: OutboxEventService,
     private val kafkaProducer: KafkaProducer
 ) {
-    private val logger = LoggerFactory.getLogger(OutboxAfterEventListener::class.java)
+    private val logger = LoggerFactory.getLogger(OutboxEventListener::class.java)
 
-    // @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    @EventListener
+    fun handleOutboxEvent(event: OutboxEventInfo) {
+        logger.info("OUTBOX_EVENT:LISTENER:BEFORE_COMMIT: $event")
+        outboxEventService.insertOrUpdate(event.toEntity())
+    }
+
+    @Async
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     fun publish(event: OutboxEventInfo) {
         try {
             logger.info("OUTBOX_EVENT:LISTENER:AFTER_COMMIT $event")
